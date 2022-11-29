e554daec8259d9b75cd5dbb40e5314f8352703ab
m250114 <115736634+m250114@users.noreply.github.com>
2022-11-28 23:21:23 -0500

    Commit Checkpoint... Images scalled scroll WIP

--- bill.py
+++ bill.py
@@ -1,19 +1,17 @@
 import pygame
-from pygame.sprite import Sprite
-#from pygame.locals import *
-#Addtional moduels in pygame
 
 pygame.init()
 
-clock= pygame.time.Clock() #locks the frames per second
+
+#locks the frames per second
+clock= pygame.time.Clock()
 fps=40
 
-#create game screen
+#create game screen dimensions
+screen_width=450
+screen_height=375
 
-screen_width=640
-screen_height=450
-#due to sprites
-
+#creates blank game window with a caption
 screen= pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Flappy Bill... USNA Edition')
 
@@ -24,89 +22,41 @@ flying= False
 game_over= False
 
 #load images
-bg = pygame.image.load('images/NA_BG2.bmp') #must load images first then blit(Draw)
-ground_img= pygame.image.load('images/waves.PNG')
-
-# class Bill(Sprite):
-#     def __init__(self, x, y):
-#         super().__init__()
-#         #pygame.sprite.Sprite.__init__(self)
-#         self.image=pygame.image.load('images/goat.png')
-#         self.index=0
-#         self.counter=0
-#         self.image=self.image[self.index]
-#         self.rect= self.image.get_rect()
-#         self.rect.center= [x,y]
-#         self.vel=0 # Acounting for the velocity and gravity
-#         self.clicked= False
-
-    # def update(self):
-    #
-    #     if flying== True:
-    #     #Gravity
-    #         self.vel += 0.5 #so bird is accelerating
-    #     if self.vel>8:
-    #         self.vel=8
-    #     if self.rect.bottom < 330:
-    #         self.rect.y +=int(self.vel) # this is a float so it must be wrapped in int function
-    #
-    #     #jump
-    #     if pygame.mouse.get_pressed()[0]==1 and self.clicked== False:
-    #         self.clicked= True
-    #         self.vel= -10
-    #     if pygame.mouse.get_pressed()[0] == 0:
-    #         self.clicked = False
-    #
-    #     #roatate the goat
-    #         self.image= pygame.transform.roatate(self.images[self.index], self.vel * -2)
-    #     else:
-    #         self.image = pygame.transform.roatate(self.images[self.index], -90)
-
-#keeps track of all of sprites similar to a python list
-# goat_group= Bill(20, 20)
-#     #pygame.sprite.Group()
-# print(goat_group)
-
-#flappy= Bill(100,(int(screen_height/2))
-
-#goat_group.add(flappy)
-
-
+#must load images first then blit(Draw)
+bg = pygame.image.load('images/BG (2).png')
+ground_img= pygame.image.load('images/cloud.jpg')
 
 #Create game loop
-run= True #initial condition that we set to true
-
-while run: #While run is true carry out all below
+#While run is true carry out all below
+run= True
+while run:
     #event handlers in pygame acklowdge user input
 
     clock.tick(fps)
 
     screen.blit(bg,(0,0)) #function to show it on the screen
 
-    # goat_group.draw(screen)
-    # goat_group.update()
 
     #draw the ground
-    screen.blit(ground_img, (ground_scroll, 330))
-
-    #Check if goat has hit the ground
-    # if flappy.rect.bottom> 330:
-    #     game_over=True
-    #     flying=False
-
+    screen.blit(ground_img,(ground_scroll, 330))
 
     if game_over== False:
         #draw and scroll the screen
         ground_scroll-= scroll_speed
         if abs(ground_scroll) >300: #gives impression of scrolling backgorund from end of image
             ground_scroll= 0
-    #
-    for event in pygame.event.get(): # This will get all the events that are happening
-            if event.type==pygame.QUIT: #looks for a paticular event
-                run= False #no longer meets required condition in the while loop
+
+     # This will get all the events that are happening
+    for event in pygame.event.get():
+            # looks for a paticular event
+            if event.type==pygame.QUIT:
+                # no longer meets required condition in the while loop
+                run= False
             if event.type== pygame.MOUSEBUTTONDOWN and flying == False and game_over== False:
                 flying= True
-    pygame.display.update() #updates for various changes in the while loop
+
+    # updates for various changes in the while loop
+    pygame.display.update()
 
 
 pygame.quit()
